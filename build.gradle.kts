6e760b5591ba6904b699dba6ca6f6184eef18167
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2021-08-01 09:45:54 +0200

    Add publishing to maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,15 @@
 group = "me.kcybulski.nexum"
-version = "1.0-SNAPSHOT"
+version = "1.0.1"
 
+plugins {
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
+}
--- event-store/build.gradle.kts
+++ event-store/build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
+    `java-library`
+    `maven-publish`
+    signing
     kotlin("jvm") version "1.5.0-RC"
 }
 
 group = "me.kcybulski.nexum"
-version = "1.0-SNAPSHOT"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
@@ -18,4 +21,46 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "me.kcybulski.nexum"
+            artifactId = "event-store"
+            from(components["java"])
+            pom {
+                name.set("Nexum")
+                description.set("Kotlin Events Store")
+                url.set("https://kcybulski.me/")
+                developers {
+                    developer {
+                        id.set("krzysztofcybulski")
+                        name.set("Krzysztof Cybulski")
+                        email.set("krzysztofpcy@gmail.com")
+                    }
+                }
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                    scm {
+                        connection.set("scm:git:git@github.com:krzysztofcybulski/nexum.git")
+                        developerConnection.set("scm:git:git@github.com:krzysztofcybulski/nexum.git")
+                        url.set("https://github.com/krzysztofcybulski/nexum/")
+                    }
+                }
+            }
+        }
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
