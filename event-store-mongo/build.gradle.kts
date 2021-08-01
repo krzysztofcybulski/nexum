597471d04f257ad8c405e6f9c597dbd10be2d91d
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2021-08-01 12:49:11 +0200

    Publishing event-store-mongo

--- event-store-mongo/build.gradle.kts
+++ event-store-mongo/build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
+    `java-library`
+    `maven-publish`
+    signing
     kotlin("jvm") version "1.5.30-M1"
 }
 
 group = "me.kcybulski.nexum"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -22,4 +25,46 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "me.kcybulski.nexum"
+            artifactId = "event-store-mongo"
+            from(components["java"])
+            pom {
+                name.set("Nexum")
+                description.set("Kotlin Events Store")
+                url.set("https://kcybulski.me/")
+                developers {
+                    developer {
+                        id.set("krzysztofcybulski")
+                        name.set("Krzysztof Cybulski")
+                        email.set("krzysztofpcy@gmail.com")
+                    }
+                }
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                    scm {
+                        connection.set("scm:git:git@github.com:krzysztofcybulski/nexum.git")
+                        developerConnection.set("scm:git:git@github.com:krzysztofcybulski/nexum.git")
+                        url.set("https://github.com/krzysztofcybulski/nexum/")
+                    }
+                }
+            }
+        }
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
